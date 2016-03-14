01d85755b6e6ccff525b988919d0ce080e4204f5
Nawhal <nawhal.sayarh@etu.udamail.fr>
2016-03-14 17:33:33 +0100

    BDD fonctionnelle et propre
    :)

--- VraiFauxAndroid/app/src/main/java/iut/info63/vraifauxandroid/activity/GameActivity.java
+++ VraiFauxAndroid/app/src/main/java/iut/info63/vraifauxandroid/activity/GameActivity.java
@@ -11,7 +11,8 @@ import java.io.IOException;
 
 import iut.info63.vraifauxandroid.R;
 import iut.info63.vraifauxandroid.metier.database.DataBaseHelper;
-import iut.info63.vraifauxandroid.metier.database.QuestionGateway;
+import iut.info63.vraifauxandroid.metier.database.DatabaseQuestionAccessor;
+import iut.info63.vraifauxandroid.metier.database.FakeQuestionAccessor;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -22,8 +23,6 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
-        int test = 0;
 
         mButtonTrue = (Button)findViewById(R.id.b_true);
         mButtonFalse = (Button)findViewById(R.id.b_false);
@@ -58,44 +57,19 @@ public class GameActivity extends AppCompatActivity {
                 mButtonFalse.setVisibility(View.VISIBLE);
             }
         });
-
-        try {
-            test();
-        }
-        catch (Exception e)
-        {
-            Log.i("COUCOU", "Exception");
-            e.printStackTrace();
-        }
     }
 
     private void test() throws IOException
     {
         DataBaseHelper dbh = new DataBaseHelper(this);
 
-        QuestionGateway qg = new QuestionGateway(dbh);
-        qg.testDeleteAll();
-        qg.putQuestion(0, "Oui.", true);
-        qg.putQuestion(1, "Non.", false);
-        qg.putQuestion(2, "Non2.", false);
-        qg.putQuestion(3, "Oui2.", true);
-        qg.putQuestion(4, "Oui3.", true);
-        qg.putQuestion(5, "Oui4.", true);
-        qg.putQuestion(6, "Non3.", false);
-        qg.putQuestion(7, "Non4.", false);
-        qg.putQuestion(8, "Non5.", false);
-        qg.putQuestion(9, "Oui5.", true);
+        DatabaseQuestionAccessor dqg = new DatabaseQuestionAccessor(dbh);
+        FakeQuestionAccessor fqg = new FakeQuestionAccessor();
 
-        Log.i("COUCOU", "Question n°1 " + qg.getQuestionByIndex(0));
-        Log.i("COUCOU", "Question n°2 " + qg.getQuestionByIndex(1));
-        Log.i("COUCOU", "Question n°3 " + qg.getQuestionByIndex(2));
-        Log.i("COUCOU", "Question n°4 " + qg.getQuestionByIndex(3));
-        Log.i("COUCOU", "Question n°5 " + qg.getQuestionByIndex(4));
-        Log.i("COUCOU", "Question n°6 " + qg.getQuestionByIndex(5));
-        Log.i("COUCOU", "Question n°7 " + qg.getQuestionByIndex(6));
-        Log.i("COUCOU", "Question n°8 " + qg.getQuestionByIndex(7));
-        Log.i("COUCOU", "Question n°9 " + qg.getQuestionByIndex(8));
-        Log.i("COUCOU", "Question n°10 " + qg.getQuestionByIndex(9));
+        dqg.testDeleteAll();
+        dqg.putAll(fqg.getAll());
+        Log.i("COUCOU", "Database : " + dqg.getAll().toString());
+        Log.i("COUCOU", "Fake : " + fqg.getAll().toString());
 
         dbh.close();
     }
---
+++ VraiFauxAndroid/app/src/main/java/iut/info63/vraifauxandroid/metier/database/DatabaseQuestionAccessor.java
@@ -0,0 +1,115 @@
+package iut.info63.vraifauxandroid.metier.database;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import iut.info63.vraifauxandroid.metier.Question;
+
+/**
+ * Created by Nawhal on 14/03/2016.
+ */
+public class DatabaseQuestionAccessor implements IQuestionGetter {
+
+    private DataBaseHelper dataBaseHelper;
+
+    public DatabaseQuestionAccessor(DataBaseHelper dataBaseHelper)
+    {
+        this.dataBaseHelper = dataBaseHelper;
+    }
+
+    @Override
+    public Question getByIndex(int index) {
+        dataBaseHelper.openReadable();
+
+        String[] projection = {
+                FeedReaderContract.QuestionsFeedEntry.FIRST_COLUMN_NAME,
+                FeedReaderContract.QuestionsFeedEntry.SECOND_COLUMN_NAME,
+                FeedReaderContract.QuestionsFeedEntry.THIRD_COLUMN_NAME
+        };
+
+        Cursor c = dataBaseHelper.getDatabase().query(
+                FeedReaderContract.QuestionsFeedEntry.TABLE_NAME,  // The table to query
+                projection,                               // The columns to return
+                FeedReaderContract.QuestionsFeedEntry.FIRST_COLUMN_NAME + "=" + index,
+                null,
+                null,
+                null,
+                null                                 // The sort order
+        );
+
+        c.moveToFirst();
+        return new Question(c.getString(c.getColumnIndexOrThrow(FeedReaderContract.QuestionsFeedEntry.SECOND_COLUMN_NAME)),
+                Boolean.parseBoolean(c.getString(c.getColumnIndexOrThrow(FeedReaderContract.QuestionsFeedEntry.THIRD_COLUMN_NAME))));
+    }
+
+    @Override
+    public List<Question> getAll() {
+        dataBaseHelper.openReadable();
+
+        String[] projection = { "*" };
+
+        Cursor c = dataBaseHelper.getDatabase().query(
+                FeedReaderContract.QuestionsFeedEntry.TABLE_NAME,  // The table to query
+                projection,                               // The columns to return
+                null,
+                null,
+                null,
+                null,
+                null                                 // The sort order
+        );
+
+        c.moveToFirst();
+
+        List<Question> questionList = new ArrayList<>();
+
+        while (!c.isAfterLast())
+        {
+            questionList.add(new Question(c.getString(c.getColumnIndexOrThrow(FeedReaderContract.QuestionsFeedEntry.SECOND_COLUMN_NAME)),
+                    Boolean.parseBoolean(c.getString(c.getColumnIndexOrThrow(FeedReaderContract.QuestionsFeedEntry.THIRD_COLUMN_NAME)))));
+            c.moveToNext();
+        }
+
+        return questionList;
+    }
+
+    @Override
+    public void put(Question question) {
+        dataBaseHelper.openWritable();
+
+        ContentValues values = new ContentValues();
+        values.put(FeedReaderContract.QuestionsFeedEntry.SECOND_COLUMN_NAME, question.getQuestion());
+        values.put(FeedReaderContract.QuestionsFeedEntry.THIRD_COLUMN_NAME, question.getAnswer());
+
+        dataBaseHelper.getDatabase().insert(
+                FeedReaderContract.QuestionsFeedEntry.TABLE_NAME,
+                null,
+                values);
+    }
+
+    @Override
+    public void putAll(List<Question> questionList) {
+        dataBaseHelper.openWritable();
+        ContentValues values;
+
+        for (Question question : questionList)
+        {
+            values = new ContentValues();
+            values.put(FeedReaderContract.QuestionsFeedEntry.SECOND_COLUMN_NAME, question.getQuestion());
+            values.put(FeedReaderContract.QuestionsFeedEntry.THIRD_COLUMN_NAME, question.getAnswer());
+
+            dataBaseHelper.getDatabase().insert(
+                    FeedReaderContract.QuestionsFeedEntry.TABLE_NAME,
+                    null,
+                    values);
+        }
+    }
+
+    public void testDeleteAll()
+    {
+        dataBaseHelper.openWritable();
+        dataBaseHelper.getWritableDatabase().delete(FeedReaderContract.QuestionsFeedEntry.TABLE_NAME, null, null);
+    }
+}
